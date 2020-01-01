94cf6dc1171246d125ac04a185935ee7678eddda
William D. Jones <thor0505@comcast.net>
2020-01-01 02:20:22 -0500

    Fix missing import in empty example.

--- examples/empty.rs
+++ examples/empty.rs
@@ -5,6 +5,12 @@ extern crate panic_msp430;
 
 use msp430_rt::entry;
 
+#[allow(unused)]
+// Bring interrupt vectors into scope so the linker can see them; enabling the "rt"
+// feature of msp430g2553 transitively enables the "device" feature of msp430-rt.
+// This prevents default interrupt vectors from being generated.
+use msp430g2553;
+
 #[entry]
 fn main() -> ! {
     loop { }
