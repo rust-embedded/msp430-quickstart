29600c92156be8fc94f390ba0bf02015e0b58946
William D. Jones <thor0505@comcast.net>
2020-01-01 23:15:11 -0500

    Add timer-oncecell example as comparison to RefCell access to peripherals.

---
+++ examples/timer-oncecell.rs
@@ -0,0 +1,65 @@
+#![no_main]
+#![no_std]
+#![feature(abi_msp430_interrupt)]
+
+extern crate panic_msp430;
+
+use once_cell::unsync::OnceCell;
+use msp430::interrupt as mspint;
+use msp430_rt::entry;
+use msp430g2553::{interrupt, Peripherals};
+
+static PERIPHERALS : mspint::Mutex<OnceCell<Peripherals>> =
+    mspint::Mutex::new(OnceCell::new());
+
+#[entry]
+fn main() -> ! {
+    let p = Peripherals::take().unwrap();
+
+    let wdt = &p.WATCHDOG_TIMER;
+    wdt.wdtctl.write(|w| {
+        unsafe { w.bits(0x5A00) } // password
+        .wdthold().set_bit()
+    });
+
+    let port_1_2 = &p.PORT_1_2;
+    port_1_2.p1dir.modify(|_, w| w.p0().set_bit()
+                                  .p6().set_bit());
+    port_1_2.p1out.modify(|_, w| w.p0().set_bit()
+                                  .p6().clear_bit());
+
+    let clock = &p.SYSTEM_CLOCK;
+    clock.bcsctl3.modify(|_, w| w.lfxt1s().lfxt1s_2());
+    clock.bcsctl1.modify(|_, w| w.diva().diva_1());
+
+    let timer = &p.TIMER0_A3;
+    timer.ta0ccr0.write(|w| unsafe { w.bits(1200) });
+    timer.ta0ctl.modify(|_, w| w.tassel().tassel_1()
+                                .mc().mc_1());
+    timer.ta0cctl1.modify(|_, w| w.ccie().set_bit());
+    timer.ta0ccr1.write(|w| unsafe { w.bits(600) });
+
+    mspint::free(|cs| {
+        PERIPHERALS.borrow(cs).set(p).ok().unwrap();
+    });
+
+    unsafe {
+        mspint::enable();
+    }
+
+    loop {}
+}
+
+#[interrupt]
+fn TIMER0_A1() {
+    mspint::free(|cs| {
+        let p = PERIPHERALS.borrow(&cs).get().unwrap();
+
+        let timer = &p.TIMER0_A3;
+        timer.ta0cctl1.modify(|_, w| w.ccifg().clear_bit());
+
+        let port_1_2 = &p.PORT_1_2;
+        port_1_2.p1out.modify(|r, w| w.p0().bit(!r.p0().bit())
+                                      .p6().bit(!r.p6().bit()));
+    });
+}
